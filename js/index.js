8826e40b5bbd06223340a3b6df341753792939a8
Renato Martins dos Santos <renato2r@gmail.com>
2025-02-11 21:12:51 -0800

    API fetch completed

--- js/index.js
+++ js/index.js
@@ -61,3 +61,40 @@ messageForm.addEventListener("submit", function (event){
 })
 
 /********************************************************************************************/
+
+/*************************************** API - FETCH - Projects ****************************************/
+
+const projectSection = document.getElementById("Projects");
+const projectList = projectSection.querySelector("ul");
+
+
+/*Creating fetch*/
+const GITHUB_USERNAME = "renato2r"; // Username
+
+fetch(`https://api.github.com/users/renato2r/repos`) //url api
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`Error ${response.status}: Could not retrieve repositories`);
+    }
+    return response.json(); // Convert response to JSON
+  })
+  .then(repositories => {
+    if (repositories.length === 0) {
+      console.log("No repositories found");
+    } else {
+      console.log("Repositories loaded successfully:", repositories);
+      repositories.forEach(repo => {
+        const project = document.createElement("li");
+        project.innerHTML = `<a href="${repo.html_url}" target="_blank">${repo.name}</a>`;
+        projectList.appendChild(project);
+      });
+    }
+  })
+  .catch(error => {
+    console.error("An error occurred while fetching the repositories:", error);
+  });
+
+
+
+
+
